0fb50b5f3b078177716bf3f6a86f07878e00b447
Clint Shryock <clint@ctshryock.com>
2017-11-01 10:46:13 -0500

    fill out helix some

---
+++ service/helix/client.go
@@ -0,0 +1,215 @@
+package helix
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+
+	"github.com/ajg/form"
+	twitch "github.com/catsby/go-twitch/twitch"
+	cleanhttp "github.com/hashicorp/go-cleanhttp"
+)
+
+// AccessTokenEnvVar is the name of the environment variable where the Twitch API
+// key should be read from.
+// ClientIdEnvVar is the name of the environment variable the client id should
+// be read from.
+const AccessTokenEnvVar = "TWITCH_ACCESS_TOKEN"
+
+// Probably not needed, but offered
+const ClientIdEnvVar = "TWITCH_CLIENT_ID"
+
+// Not used yet
+const ClientSecretEnvVar = "TWITCH_CLIENT_SECRET"
+
+// AccessTokenHeader is the name of the header that contains the Twitch API key.
+const AccessTokenHeader = "Authorization"
+const ClientIdHeader = "Client-ID"
+
+type Client struct {
+	Config *twitch.Config
+
+	// HTTPClient is the HTTP client to use. If one is not provided, a default
+	// client will be used.
+	httpClient *http.Client
+
+	// accessToken is the Twitch API key to authenticate requests.
+	accessToken string
+
+	// clientId is the Twitch Application Client ID to authenticate requests.
+	// Register your application here:
+	//   https://dev.twitch.tv/docs/v5/guides/authentication/#registration
+	// Note: probably not needed for general API consumption
+	clientId string
+
+	clientSecret string
+
+	// url is the parsed URL from Address
+	// ??
+	url *url.URL
+}
+
+// type Client twitch.Config
+
+// DefaultClient instantiates a new Twitch API client. This function requires
+// the environment variables `TWITCH_ACCESS_TOKEN` and `TWITCH_CLIENT_ID` to be
+// set and contain valid access token and client id, respectively, to
+// authenticate with Twitch.
+func DefaultClient(config *twitch.Config) (*Client, error) {
+	// TODO not even really used right now you should fix this, it's unused
+	// because I'm refactoring
+	if config == nil {
+		config = &twitch.Config{}
+	}
+
+	if config.ClientId == "" {
+		config.ClientId = os.Getenv(ClientIdEnvVar)
+	}
+	if config.AccessToken == "" {
+		log.Printf("\t setting access token \n")
+		config.AccessToken = os.Getenv(AccessTokenEnvVar)
+	}
+	if config.Endpoint == "" {
+		config.Endpoint = twitch.DefaultEndpoint
+	}
+	if config.ClientSecret == "" {
+		config.ClientSecret = os.Getenv(ClientIdEnvVar)
+	}
+
+	client, err := NewClient(config)
+	if err != nil {
+		log.Printf("Failed to make default helix client: %s", err)
+		return nil, err
+	}
+
+	return client, nil
+}
+
+// NewClient creates a new API client with the given key and the default API
+// endpoint. Twitch requires both an access token and a client id for requests,
+// so we error if either is empty.
+//
+// Creating an access token is not yet supported by this libary
+// TODO: Support creating an access token
+func NewClient(config *twitch.Config) (*Client, error) {
+	if config.AccessToken == "" {
+		return nil, fmt.Errorf("Access Token not specified")
+	}
+
+	if config.HTTPClient == nil {
+		config.HTTPClient = cleanhttp.DefaultClient()
+	}
+
+	c := &Client{
+		accessToken:  config.AccessToken,
+		clientId:     config.ClientId,
+		httpClient:   config.HTTPClient,
+		clientSecret: config.ClientSecret,
+	}
+
+	u, err := url.Parse(config.Endpoint)
+	if err != nil {
+		return nil, err
+	}
+
+	c.url = u
+
+	return c, nil
+}
+
+func (c *Client) Get(p string, ro *twitch.RequestOptions) (*http.Response, error) {
+	return c.Request("GET", p, ro)
+}
+
+// Head issues an HTTP HEAD request.
+func (c *Client) Head(p string, ro *twitch.RequestOptions) (*http.Response, error) {
+	return c.Request("HEAD", p, ro)
+}
+
+// Post issues an HTTP POST request.
+func (c *Client) Post(p string, ro *twitch.RequestOptions) (*http.Response, error) {
+	return c.Request("POST", p, ro)
+}
+
+// PostForm issues an HTTP POST request with the given interface form-encoded.
+func (c *Client) PostForm(p string, i interface{}, ro *twitch.RequestOptions) (*http.Response, error) {
+	return c.RequestForm("POST", p, i, ro)
+}
+
+// Put issues an HTTP PUT request.
+func (c *Client) Put(p string, ro *twitch.RequestOptions) (*http.Response, error) {
+	return c.Request("PUT", p, ro)
+}
+
+// PutForm issues an HTTP PUT request with the given interface form-encoded.
+func (c *Client) PutForm(p string, i interface{}, ro *twitch.RequestOptions) (*http.Response, error) {
+	return c.RequestForm("PUT", p, i, ro)
+}
+
+// Delete issues an HTTP DELETE request.
+func (c *Client) Delete(p string, ro *twitch.RequestOptions) (*http.Response, error) {
+	return c.Request("DELETE", p, ro)
+}
+
+// Request makes an HTTP request against the HTTPClient using the given verb,
+// Path, and request options.
+func (c *Client) Request(verb, p string, ro *twitch.RequestOptions) (*http.Response, error) {
+	req, err := c.RawRequest(verb, p, ro)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := checkResp(c.httpClient.Do(req))
+	if err != nil {
+		return resp, err
+	}
+
+	return resp, nil
+}
+
+// RequestForm makes an HTTP request with the given interface being encoded as
+// form data.
+func (c *Client) RequestForm(verb, p string, i interface{}, ro *twitch.RequestOptions) (*http.Response, error) {
+	if ro == nil {
+		ro = new(twitch.RequestOptions)
+	}
+
+	if ro.Headers == nil {
+		ro.Headers = make(map[string]string)
+	}
+	ro.Headers["Content-Type"] = "application/x-www-form-urlencoded"
+	// log.Printf("headers:\n%s\n", spew.Sdump(ro.Headers))
+
+	buf := new(bytes.Buffer)
+	if err := form.NewEncoder(buf).KeepZeros(true).DelimitWith('|').Encode(i); err != nil {
+		return nil, err
+	}
+	body := buf.String()
+
+	ro.Body = strings.NewReader(body)
+	ro.BodyLength = int64(len(body))
+
+	return c.Request(verb, p, ro)
+}
+
+// checkResp wraps an HTTP request from the default client and verifies that the
+// request was successful. A non-200 request returns an error formatted to
+// included any validation problems or otherwise.
+func checkResp(resp *http.Response, err error) (*http.Response, error) {
+	// If the err is already there, there was an error higher up the chain, so
+	// just return that.
+	if err != nil {
+		return resp, err
+	}
+
+	switch resp.StatusCode {
+	case 200, 201, 202, 204, 205, 206:
+		return resp, nil
+	default:
+		return resp, twitch.NewHTTPError(resp)
+	}
+}
---
+++ service/helix/client_test.go
@@ -0,0 +1,172 @@
+package helix
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/catsby/go-twitch/twitch"
+)
+
+func TestHelixClient_DefaultClient(t *testing.T) {
+
+	cases := []struct {
+		Label       string
+		Envs        []string
+		Endpoint    string
+		Config      *twitch.Config
+		ShouldFail  bool
+		ShouldError bool
+	}{
+		{
+			Label: "Default with 1 TWITCH_ACCESS_TOKEN set",
+			Envs:  []string{"TWITCH_ACCESS_TOKEN"},
+		},
+		{
+			Label:  "Default with 1 TWITCH_ACCESS_TOKEN set, overwriting Config",
+			Envs:   []string{"TWITCH_ACCESS_TOKEN"},
+			Config: &twitch.Config{AccessToken: "blah"},
+		},
+		{
+			Label:  "No env",
+			Envs:   []string{},
+			Config: &twitch.Config{AccessToken: "blah"},
+		},
+	}
+
+	env := make(map[string]string)
+	env["TWITCH_CLIENT_SECRET"] = "client_secret_123"
+	env["TWITCH_ACCESS_TOKEN"] = "access_token_123"
+	env["TWITCH_CLIENT_ID"] = "client_id_123"
+
+	resetEnv := unsetEnv(t)
+	defer resetEnv()
+	for _, c := range cases {
+		t.Run(fmt.Sprintf("%s", c.Label), func(t *testing.T) {
+			unsetEnv(t)
+			var accessToken string
+			var clientId string
+			var clientSecret string
+			// var endpoint string
+			if c.Config != nil {
+				accessToken = c.Config.AccessToken
+				clientId = c.Config.ClientId
+				// endpoint = c.Config.Endpoint
+			}
+
+			for _, e := range c.Envs {
+				if v, ok := env[e]; ok {
+					os.Setenv(e, v)
+					switch e {
+					case "TWITCH_ACCESS_TOKEN":
+						accessToken = env[e]
+					case "TWITCH_CLIENT_SECRET":
+						clientSecret = env[e]
+					case "TWITCH_CLIENT_ID":
+						clientId = env[e]
+					}
+				}
+			}
+
+			h, err := DefaultClient(c.Config)
+			if err != nil {
+				if !c.ShouldError {
+					t.Fatalf("Error making default client when error not expected")
+				}
+			}
+
+			var tErr error
+			if h.accessToken != accessToken {
+				tErr = fmt.Errorf("AccessToken does not match")
+			}
+			if h.clientId != clientId {
+				tErr = fmt.Errorf("clientId does not match")
+			}
+			if h.clientSecret != clientSecret {
+				tErr = fmt.Errorf("clientSecret does not match")
+			}
+			// if h.endpoint != endpoint {
+			// 	t.Fatalf("Endpoint does not match")
+			// }
+
+			if c.ShouldFail && tErr == nil {
+				log.Printf("should fail: %t\n", c.ShouldFail)
+				log.Printf("tErr: %s\n", tErr)
+				t.Fatalf("we should have failed by now")
+			}
+		})
+	}
+}
+
+// unsetEnv unsets environment variables for testing a "clean slate" with no
+// credentials in the environment
+func unsetEnv(t *testing.T) func() {
+	// Grab any existing Twitch keys and preserve. In some tests we'll unset these, so
+	// we need to have them and restore them after
+	e := getEnv()
+	if err := os.Unsetenv("TWITCH_CLIENT_SECRET"); err != nil {
+		t.Fatalf("Error unsetting env var TWITCH_CLIENT_SECRET: %s", err)
+	}
+	if err := os.Unsetenv("TWITCH_CLIENT_ID"); err != nil {
+		t.Fatalf("Error unsetting env var TWITCH_CLIENT_ID: %s", err)
+	}
+	if err := os.Unsetenv("TWITCH_ACCESS_TOKEN"); err != nil {
+		t.Fatalf("Error unsetting env var TWITCH_ACCESS_TOKEN: %s", err)
+	}
+
+	return func() {
+		// re-set all the envs we unset above
+		if err := os.Setenv("TWITCH_CLIENT_SECRET", e.ClientSecret); err != nil {
+			t.Fatalf("Error resetting env var TWITCH_CLIENT_SECRET: %s", err)
+		}
+		if err := os.Setenv("TWITCH_CLIENT_ID", e.ClientId); err != nil {
+			t.Fatalf("Error resetting env var TWITCH_CLIENT_ID: %s", err)
+		}
+		if err := os.Setenv("TWITCH_ACCESS_TOKEN", e.AccessToken); err != nil {
+			t.Fatalf("Error resetting env var TWITCH_ACCESS_TOKEN: %s", err)
+		}
+	}
+}
+
+func setEnv(s string, t *testing.T) func() {
+	e := getEnv()
+	// Set all the envs to a dummy value
+	if err := os.Setenv("TWITCH_CLIENT_SECRET", s); err != nil {
+		t.Fatalf("Error setting env var TWITCH_CLIENT_SECRET: %s", err)
+	}
+	if err := os.Setenv("TWITCH_CLIENT_ID", s); err != nil {
+		t.Fatalf("Error setting env var TWITCH_CLIENT_ID: %s", err)
+	}
+	if err := os.Setenv("TWITCH_ACCESS_TOKEN", s); err != nil {
+		t.Fatalf("Error setting env var TWITCH_ACCESS_TOKEN: %s", err)
+	}
+
+	return func() {
+		// re-set all the envs we unset above
+		if err := os.Setenv("TWITCH_CLIENT_SECRET", e.ClientSecret); err != nil {
+			t.Fatalf("Error resetting env var TWITCH_CLIENT_SECRET: %s", err)
+		}
+		if err := os.Setenv("TWITCH_CLIENT_ID", e.ClientId); err != nil {
+			t.Fatalf("Error resetting env var TWITCH_CLIENT_ID: %s", err)
+		}
+		if err := os.Setenv("TWITCH_ACCESS_TOKEN", e.AccessToken); err != nil {
+			t.Fatalf("Error resetting env var TWITCH_ACCESS_TOKEN: %s", err)
+		}
+	}
+}
+
+func getEnv() *currentEnv {
+	// Grab any existing Twitch keys and preserve. In some tests we'll unset these, so
+	// we need to have them and restore them after
+	return &currentEnv{
+		AccessToken:  os.Getenv("TWITCH_ACCESS_TOKEN"),
+		ClientSecret: os.Getenv("TWITCH_CLIENT_SECRET"),
+		ClientId:     os.Getenv("TWITCH_CLIENT_ID"),
+	}
+}
+
+// struct to preserve the current environment
+type currentEnv struct {
+	AccessToken, ClientSecret, ClientId string
+}
