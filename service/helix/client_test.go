36de7d99efab508861fe392c6c429adb115fa394
Clint Shryock <clint@ctshryock.com>
2017-11-13 10:33:29 -0600

    copy over the recordKraken method for acc tests in helix

--- service/helix/client_test.go
+++ service/helix/client_test.go
@@ -7,6 +7,8 @@ import (
 	"testing"
 
 	"github.com/catsby/go-twitch/twitch"
+	"github.com/dnaeon/go-vcr/recorder"
+	cleanhttp "github.com/hashicorp/go-cleanhttp"
 )
 
 func TestHelixClient_DefaultClient(t *testing.T) {
@@ -169,4 +171,35 @@ func getEnv() *currentEnv {
 // struct to preserve the current environment
 type currentEnv struct {
 	AccessToken, ClientSecret, ClientId string
+}
+
+func recordHelix(t *testing.T, fixture string, f func(*Client)) {
+	modeDisabledEnv := os.Getenv("RECORD_DISABLE")
+	mode := recorder.ModeReplaying
+	if modeDisabledEnv == "true" {
+		mode = recorder.ModeDisabled
+		log.Printf("[DEBUG]: Response recording disabled")
+	}
+
+	r, err := recorder.NewAsMode("fixtures/"+fixture, mode, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		if err := r.Stop(); err != nil {
+			t.Fatal(err)
+		}
+	}()
+
+	config := twitch.Config{
+		HTTPClient: cleanhttp.DefaultClient(),
+	}
+	config.HTTPClient.Transport = r
+
+	client, err := DefaultClient(&config)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	f(client)
 }
